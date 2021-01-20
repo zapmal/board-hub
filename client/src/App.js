4a4fd3e15ec65d71ce4eeaf1150e75d374c3a0d8
Zondazx <mzxgd16@gmail.com>
2021-01-20 13:06:23 -0400

    chore: add more packages and main logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo from './images/logo.png';
 
 const App = () => {
   return (
     <div>
       <header>
         <h4>Hey</h4>
+        <img src={logo} />
       </header>
     </div>
   );
