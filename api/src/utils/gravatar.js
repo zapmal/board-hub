173d995814045bd890d9227176b7d8e34ab7fb79
Zondazx <mzxgd16@gmail.com>
2021-01-23 14:52:26 -0400

    feat: add gravatar util and api service

---
+++ api/src/utils/gravatar.js
@@ -0,0 +1,8 @@
+const md5 = require('md5');
+
+const gravatar = (email) => {
+  const hash = md5(email);
+  return `https://www.gravatar.com/avatar/${hash}.jpg?d=identicon`;
+};
+
+module.exports = gravatar;
