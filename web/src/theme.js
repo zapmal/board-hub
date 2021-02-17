d0a8d56f47c4b5354956c2eff10ca5d56ec8e6b1
Zondazx <mzxgd16@gmail.com>
2021-02-17 16:38:17 -0400

    fix: remove darkTheme from theme

--- web/src/theme.js
+++ web/src/theme.js
@@ -1,10 +1,5 @@
 import { createMuiTheme } from '@material-ui/core';
 
-const extraColors = {
-  lightDark: '#5e5756',
-  darkBlue: '#2F3342',
-};
-
 export const theme = createMuiTheme({
   palette: {
     primary: {
@@ -15,23 +10,9 @@ export const theme = createMuiTheme({
       main: '#7362d0',
       dark: '#504491',
     },
-    extraColors: extraColors,
-  },
-});
-
-export const darkTheme = createMuiTheme({
-  palette: {
-    type: 'dark',
-    primary: {
-      light: '#ffffff',
-      main: '#ffffff',
-      dark: '#b2b2b2',
+    extraColors: {
+      lightDark: '#5e5756',
+      darkBlue: '#2F3342',
     },
-    secondary: {
-      light: '#5b5959',
-      main: '#333030',
-      dark: '#232121',
-    },
-    extraColors: extraColors,
   },
 });
