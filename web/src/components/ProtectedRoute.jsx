70b3880a46289822b8ed91a4c821b1747b2d19ec
Zondazx <mzxgd16@gmail.com>
2021-02-19 15:10:01 -0400

    fix: bug that allows unathorized users to access board pages

--- web/src/components/ProtectedRoute.jsx
+++ web/src/components/ProtectedRoute.jsx
@@ -1,30 +1,16 @@
-
 import React from 'react';
 import { Route, Redirect, } from 'react-router-dom';
 
 import useUserStore from '../stores/useUserStore';
 
-/**
- * This components does a few things.
- * 
- * 1.- Check if there's an active session, if there is, 
- * said user can access to *any* protected route (/b, /account). 
- * 
- * 2.- Checks if there's a token (if he logged in previously), if so,
- * he's redirected to home page, where its data is retrieved and its session
- * established.
- * 
- * 3.- The else-if is to avoid an infinite loop.
- * e.g: User is not logged in, there's no active session so he'll go and signup/signin,
- * but he'll be redirect over and over, infinite loop.
- */
 const ProtectedRoute = ({ path, exact, route }) => {
   const activeSession = useUserStore(state => state.user);
   const isLoggedIn = localStorage.getItem('token');
+  const isAccessPage = (path === '/signin' || path === '/signup');
 
-  if (isLoggedIn && (path === '/signin' || path === '/signup')) {
+  if (isLoggedIn && isAccessPage) {
     return <Redirect to= '/' />;
-  } else if (!isLoggedIn) {
+  } else if (!isLoggedIn && isAccessPage) {
     return (
       <Route 
         path={path}
