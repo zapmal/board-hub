36a9844c84553493ad0133c39e0a343e89cc91b1
Zondazx <mzxgd16@gmail.com>
2021-02-28 19:41:04 -0400

    refactor: better imports

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ThemeProvider } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
 import { QueryClientProvider, QueryClient } from 'react-query';
 
 import GlobalStyle from './components/GlobalStyle';
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Layout from '../components/Layout';
