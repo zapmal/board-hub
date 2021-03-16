4bc380385982373cf35dca508c62f82bcacad704
Zondazx <mzxgd16@gmail.com>
2021-03-16 18:36:42 -0400

    refactor: move logic to controller

--- api/src/components/lists/listsController.js
+++ api/src/components/lists/listsController.js
@@ -4,9 +4,30 @@ import {
 } from './listsService';
 
 const getLists = async (boardID, response) => {
-  const lists = await getBoardLists(boardID);
+  const [rawLists, rawCards] = await getBoardLists(boardID);
+  const data = { cards: {}, lists: {} };
 
-  return lists;
+  for (const current of rawLists) {
+    const key = current.name.toLowerCase();
+    data.lists[key] = {
+      id: key,
+      uid: current.id,
+      name: current.name,
+      order: current.order,
+      cardIds: current.listCards.map(card => card.id),
+      board_id: current.board_id,
+    };
+  }
+
+  for (const current of rawCards) {
+    const key = current.id.toString();
+    data.cards[key] = {
+      id: key,
+      title: current.title,
+    };
+  }
+
+  return data;
 };
 
 const putOrder = async (
