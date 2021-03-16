8b0351894146b5ed158b1922c4386f8a97d05192
Zondazx <mzxgd16@gmail.com>
2021-03-16 18:30:09 -0400

    fix: structured the data before sending it to the client and added some information that was missing (and needed)

--- api/src/components/lists/listsService.js
+++ api/src/components/lists/listsService.js
@@ -1,9 +1,47 @@
-import { list } from '@models';
+import { list, card } from '@models';
+import { Op } from 'sequelize';
 
 const getBoardLists = async (boardID) => {
-  const lists = await list.findAll({ where: { board_id: boardID } });
+  const rawLists = await list.findAll({
+    where: { board_id: boardID },
+    include: {
+      model: card,
+      as: 'listCards',
+      attributes: ['id', 'title'],
+    },
+  });
+  const rawCards = await card.findAll({
+    where: {
+      list_id: {
+        [Op.or]: rawLists.map(l => l.id),
+      },
+    },
+    attributes: ['id', 'title'],
+  });
 
-  return lists;
+  const data = { cards: {}, lists: {} };
+
+  for (const current of rawLists) {
+    const key = current.name.toLowerCase();
+    data.lists[key] = {
+      id: key,
+      uid: current.id,
+      name: current.name,
+      order: current.order,
+      cardIds: current.listCards.map(c => c.id),
+      board_id: current.board_id,
+    };
+  }
+
+  for (const current of rawCards) {
+    const key = current.id.toString();
+    data.cards[key] = {
+      id: key,
+      title: current.title,
+    };
+  }
+
+  return data;
 };
 
 const updateListOrder = async (id, order) => {
