e3cab913f97235d3e58cc231ef722e28d4f2a2c2
Zondazx <mzxgd16@gmail.com>
2021-03-16 18:40:31 -0400

    refactor: rename ambiguous 'current'

--- api/src/components/lists/listsController.js
+++ api/src/components/lists/listsController.js
@@ -7,23 +7,23 @@ const getLists = async (boardID, response) => {
   const [rawLists, rawCards] = await getBoardLists(boardID);
   const data = { cards: {}, lists: {} };
 
-  for (const current of rawLists) {
-    const key = current.name.toLowerCase();
+  for (const list of rawLists) {
+    const key = list.name.toLowerCase();
     data.lists[key] = {
       id: key,
-      uid: current.id,
-      name: current.name,
-      order: current.order,
-      cardIds: current.listCards.map(card => card.id),
-      board_id: current.board_id,
+      uid: list.id,
+      name: list.name,
+      order: list.order,
+      cardIds: list.listCards.map(card => card.id),
+      board_id: list.board_id,
     };
   }
 
-  for (const current of rawCards) {
-    const key = current.id.toString();
+  for (const card of rawCards) {
+    const key = card.id.toString();
     data.cards[key] = {
       id: key,
-      title: current.title,
+      title: card.title,
     };
   }
 
